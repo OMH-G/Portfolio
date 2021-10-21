98e51a1546ea15782792163bbb7d010fc075008d
Omkar <omkarhalgi50@gmail.com>
2021-10-21 12:16:33 +0530

    merge 2 completed

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,33 +4,11 @@ export default function Info() {
     return (
         <>
             <div class="three">
-<<<<<<< HEAD
                 <div style={{ 'alignSelf': 'baseline' }}>
                     <h1>I am a Web-Developer and <span></span></h1>
                 </div>
                 <div>
                     profile card
-=======
-            <div style={{'alignSelf':'baseline'}}>
-                <h1>I am a Web-Developer and <span></span></h1>
-            </div>
-                <div class="row">
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3" style={{'width':'inherit'}}>
-                        <div class="our-team">
-                            <div class="picture">
-                                <img alt="new" class="img-fluid" src="https://picsum.photos/130/130?image=856" />
-                            </div>
-                            <div class="team-content">
-                                <h3 class="name">Justin Ramos</h3>
-                                <h4 class="title">Web Developer</h4>
-                            </div>
-                            <ul class="social">
-                                Omkar
-                            </ul>
-                        </div>
-                    </div>
-
->>>>>>> 9a4caf4911cdc10713693f73d5b8bad614829d47
                 </div>
             </div>
 
