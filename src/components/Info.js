d000a892cd1969a44c78a8885d81e80236078515
Omkar <omkarhalgi50@gmail.com>
2021-10-22 19:38:53 +0530

    info page added

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,11 +4,24 @@ export default function Info() {
     return (
         <>
             <div class="three">
-                <div style={{ 'alignSelf': 'baseline' }}>
+                <div style={{ 'alignSelf': 'baseline', 'marginLeft': 'auto', 'marginRight': 'auto' }}>
                     <h1>I am a Web-Developer and <span></span></h1>
                 </div>
-                <div>
-                    profile card
+                <div className="flex-prof-card">
+                    <div className="info-content">
+                        <div class="two">hi folks, this is typing animation using CSS</div>
+                    </div>
+                    <div class="profile-flipper">
+                        <div class="card">
+                            <div class="front">
+                                <p>HOVER</p>
+                            </div>
+                            <div class="back">
+                                <p>BACK</p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
