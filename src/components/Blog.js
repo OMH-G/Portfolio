e88fe959c4bc78d78c45e030ad1c67449aba3032
Omkar <omkarhalgi50@gmail.com>
2021-11-03 19:43:54 +0530

    added blog.js in underconstruction and modified info.js space-between

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import '../static/css/Blog.css'
 export default function Blog() {
     return (
-        <div>
-            blog.
+        <div class="blog-html">
+            <div class="blog-body">
+                <div id="uc">
+                    <span>Blog Page is under construction</span>
+                    <br/>
+                    <div id="loading">
+                    <div id="wiggle"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
