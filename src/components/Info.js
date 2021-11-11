af872d855a480bc682831d2b6d3f76928a80dcd4
Omkar <omkarhalgi50@gmail.com>
2021-11-11 20:14:50 +0530

    skills card,deployed website added

--- src/components/Info.js
+++ src/components/Info.js
@@ -29,7 +29,83 @@ export default function Info() {
                         <div className="info-content v4">
                             <p>I dont' know dude, May be this could be important, May be not, who knows? I don't know, Do you know? Do you know somebody that knows this maybe, call me maybe.</p>
                         </div>
+                        <div class="skills-body">
+                        <div class="skill-bars">
+                        <div class="bar">
+                            <div class="info">
+                            <span>HTML</span>
+                            </div>
+                            <div class="progress-line html">
+                            <span></span>
+                            </div>
+                        </div>
+                        <div class="bar">
+                            <div class="info">
+                            <span>CSS</span>
+                            </div>
+                            <div class="progress-line css">
+                            <span></span>
+                            </div>
+                        </div>
+                        <div class="bar">
+                            <div class="info">
+                            <span>jQuery</span>
+                            </div>
+                            <div class="progress-line jquery">
+                            <span></span>
+                            </div>
+                        </div>
+                        <div class="bar">
+                            <div class="info">
+                            <span>Python</span>
+                            </div>
+                            <div class="progress-line python">
+                            <span></span>
+                            </div>
+                        </div>
+                        <div class="bar">
+                            <div class="info">
+                            <span>MySQL</span>
+                            </div>
+                            <div class="progress-line mysql">
+                            <span></span>
+                            </div>
+                        </div>
+                        </div>
+                <div style={{'display':'flex','flexWrap':'wrap','flexDirection':'column','alignItems':'center','backgroundColor':'honeydew','padding':'10px','borderRadius':'20px'}}>
+                <div>Deploys</div>
+                <div style={{'display':'flex','flexWrap':'wrap','justifyContent':'center'}}>
+                <div class="wrapper-card my-1 mx-1">
+                <div class="tiles-card root-wrapper-card">
+                    <div class="tiles-overlay">
 
+                    </div>
+                    <div class="tiles-circle">
+                    <img src="https://www.inlifehealthcare.com/wp-content/uploads/2016/03/To-Do-List.png" data-v-5e99277a=""/>
+                    <p>
+                        <a href="https://todoapplistvslist.herokuapp.com/">Enter</a>
+                    </p>
+                    </div>
+                </div>
+                </div>
+
+                <div class="wrapper-card my-1 mx-1">
+                <div class="tiles-card root-wrapper-card">
+                    <div class="tiles-overlay">
+
+                    </div>
+                    <div class="tiles-circle">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1457/1457806.png" data-v-5e99277a=""/>
+                    <p>
+                        <a href="https://bankrupthone.herokuapp.com/">Enter</a>
+                    </p>
+                    </div>
+                </div>
+                </div>
+
+                </div>
+                </div>
+                </div>
                     </div>
 
                 </div>
